ab98ad7a58316d94dce6c2e3506fdb2c9e04dd26
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-11-19 08:30:21 +0530

    fetch data through API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import axios from 'axios'
+import {useState} from 'react'
 
-function App() {
-  return (
+const App = () => {
+
+  const [username,setUsername] = useState('')
+
+  const updateUsername=(e)=>{
+    setUsername(e.target.value)
+  }
+
+  const getDetails=()=>{
+    axios.get('https://api.github.com/users/adityabisoi/repos')
+      .then(data=>console.log(data))
+  }
+
+  const sendUsername=(e)=>{
+    e.preventDefault()
+    getDetails(username)
+  }
+
+  return(
     <div className="App">
-      <h1>Hello</h1>
+      <form onSubmit={sendUsername}>
+        <label>Enter your github username</label>
+        <input type='text' value={username} onChange={updateUsername}/>
+        <button type='submit'>Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
