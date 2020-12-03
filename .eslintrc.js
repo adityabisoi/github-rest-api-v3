a92663d696d0623b3821df0d4fd1a302c360e538
subhangi2731 <subhangichoudhary6@gmail.comgit config --global user.name subhangi2731git config --global user.email subhangichoudhary6@gmail.com>
2020-12-03 18:43:46 +0530

    airbnb styling guide added

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "airbnb",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import"
+
+    ],
+    "rules": {
+    }
+};
