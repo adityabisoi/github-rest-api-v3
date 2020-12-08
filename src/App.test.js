bd6045f88a9f20d16317953b2805357744ff8788
subhangi2731 <subhangichoudhary6@gmail.comgit config --global user.name subhangi2731git config --global user.email subhangichoudhary6@gmail.com>
2020-12-08 18:53:17 +0530

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import Header from './components/header';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Header />);
 });
